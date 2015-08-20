3cbf7c5377bbfb5f3a25046c737b6d584a2b211f
Mike King <mike.king@digitalsurgeons.com>
2015-08-20 14:49:15 -0400

    Controllers Connected to Views
    http://localhost/projectastronaut/public/admin/new-company displays
    new.blade.php
    
    http://localhost/projectastronaut/public/admin/company-view displays
    allcompany.blade.php
    
    http://localhost/projectastronaut/public/admin/report-setup displays
    resportset.blade.php
    
    http://localhost/projectastronaut/public/display-report?id= displays
    report.blade.php
    
    http://localhost/projectastronaut/public/admin/dashboard displays
    dash.blade.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,20 +11,24 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
-Route::get('/display-report/{id}', 'DisplayReport@index');
+Route::get('/', 'Login@index');
+
+Route::get('/display-report?id={id}', 'DisplayReport@index');
 Route::get('/display-report', 'DisplayReport@unidentified');
-Route::get('/display-unknown/{id}', 'DisplayReport@unidentified');
+Route::get('/display-unknown?id={id}', 'DisplayReport@unidentified');
+Route::get('/admin/', 'Login@index');
 Route::get('/admin/login', 'Login@index');
 Route::get('/admin/report-setup/', 'ReportSetup@index');
 Route::get('/admin/company-view', 'CompanyView@index');
-Route::get('/admin/company-view-detail/{id}', 'CompanyView@detail');
+Route::get('/admin/company-view-detail?={id}', 'CompanyView@detail');
 Route::get('/admin/new-company', 'NewCompany@index');
 Route::get('/admin/upload/', 'Upload@index');
 Route::get('/admin/uploaded/', 'Upload@display');
 Route::get('/admin/all-companies/', 'AllCompanies@index');
+Route::get('/admin/dashboard/', 'dashboard@index');
 
 
