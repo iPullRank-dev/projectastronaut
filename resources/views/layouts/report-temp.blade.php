3c7fa1dc7b1eb82840707c5566318ff6796ab5c9
bertya <bert_b@live.com>
2016-08-05 15:12:38 -0400

    cookie

--- resources/views/layouts/report-temp.blade.php
+++ resources/views/layouts/report-temp.blade.php
@@ -566,6 +566,18 @@
     @yield('pagejs')
 
     <!-- END PAGE SCRIPTS -->
+    
+    <script type="text/javascript">
+  (function() {
+    window._pa = window._pa || {};
+    // _pa.orderId = "myOrderId"; // OPTIONAL: attach unique conversion identifier to conversions
+    // _pa.revenue = "19.99"; // OPTIONAL: attach dynamic purchase values to conversions
+    // _pa.productId = "myProductId"; // OPTIONAL: Include product ID for use with dynamic ads
+    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+    pa.src = ('https:' == document.location.protocol ? 'https:' : 'http:') + "//tag.marinsm.com/serve/55e9b4a0cec51a0916000224.js";
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(pa, s);
+  })();
+</script>
 
 </body>
 
